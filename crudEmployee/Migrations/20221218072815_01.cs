5a360484df85307721453e5904014c66f17e5615
Amol Phalke <amolp.anvitechsys@gmail.com>
2024-03-01 18:32:42 +0530

    Add project files.

---
+++ crudEmployee/Migrations/20221218072815_01.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace crudemployee.Migrations
+{
+    public partial class _01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    dob = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ crudEmployee/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace crudemployee.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public string address { get; set; }
+        [Required]
+        public DateTime dob { get; set; }
+    }
+}
